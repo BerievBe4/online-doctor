7a786fe588abf11cf811f898f465fb6e8a477599
Alexsandr Syrykh <sane4e55@gmail.com>
2022-03-15 20:46:43 +0300

    Вынес connection string в параметры. Частично реализовал паттерн Repository для общения с БД.

---
+++ online-doctor/Providers/IDbConnectionProvider.cs
@@ -0,0 +1,9 @@
+﻿using System.Data;
+
+namespace online_doctor.Providers
+{
+    public interface IDbConnectionProvider
+    {
+        IDbConnection Open();
+    }
+}
---
+++ online-doctor/Providers/MySqlConnectionProvider.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace online_doctor.Providers
+{
+    public class MySqlConnectionProvider : IDbConnectionProvider
+    {
+        private readonly string _connectionString;
+
+        public MySqlConnectionProvider(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("connectionStringToMySql");
+        }
+
+        public IDbConnection Open()
+        {
+            var connection = new MySqlConnection(_connectionString);
+            connection.Open();
+            return connection;
+        }
+    }
+}
