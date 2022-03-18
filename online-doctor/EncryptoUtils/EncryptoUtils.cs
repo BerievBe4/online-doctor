5c314ae1c72728cc8ab7726983a7b675cb55e55a
Alexsandr Syrykh <sane4e55@gmail.com>
2022-03-19 00:21:34 +0300

    Добавил утилиты для криптографии. Будет использоваться при шифровании персональных данных в БД.

---
+++ online-doctor/EncryptoUtils/EncryptoUtils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace online_doctor.EncryptoUtils
+{
+    public class EncryptoUtils
+    {
+        private static Random _random = new Random(Environment.TickCount);
+        private static string _alphabet = "0123456789abcdefghijklmnopqrstuvwxyz";
+
+        public static string RandomString(int length)
+        {
+            StringBuilder builder = new StringBuilder(length);
+
+            for (int i = 0; i < length; ++i)
+                builder.Append(_alphabet[_random.Next(_alphabet.Length)]);
+
+            return builder.ToString();
+        }
+    }
+}
