c3b489dc330d0b76f5d866a3474dc81196757e18
Alexsandr Syrykh <sane4e55@gmail.com>
2022-03-19 00:34:46 +0300

    Реализовал вход на сайт.

---
+++ online-doctor/Controllers/LoginController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using online_doctor.Models;
+using online_doctor.Repositories;
+
+namespace online_doctor.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly UserRepository _userRepository;
+
+        public LoginController(UserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            User existUser = _userRepository.GetUserByLogin(user.Login);
+            if (existUser == null)
+            {
+                user.ErrorMessage = "Неправильный логин или пароль.";
+                return View("Login", user);
+            }
+
+            if (!BCrypt.Net.BCrypt.Verify(user.UserPassword, existUser.UserPassword))
+            {
+                user.ErrorMessage = "Неправильный логин или пароль.";
+                return View("Login", user);
+            }
+
+            HttpContext.Session.SetString("Login", existUser.Login);
+            HttpContext.Session.SetInt32("UserID", existUser.UserId);
+            HttpContext.Session.SetInt32("RoleID", existUser.IdRole);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult LogOut()
+        {
+            string UserID = HttpContext.Session.GetString("UserID");
+
+            if (!string.IsNullOrEmpty(UserID))
+                HttpContext.Session.Clear();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
