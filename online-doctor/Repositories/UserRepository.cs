0f6c5bbf3b0478afa67fba71c258e36be280cd66
Alexsandr Syrykh <sane4e55@gmail.com>
2022-03-19 00:20:35 +0300

    Добавил UserRepository. Также заматчил функцию регистрацию в БД и в C#.

---
+++ online-doctor/Repositories/UserRepository.cs
@@ -0,0 +1,51 @@
+﻿using Dapper;
+using online_doctor.Models;
+using online_doctor.Providers;
+using Org.BouncyCastle.Crypto.Generators;
+using System.Linq;
+
+namespace online_doctor.Repositories
+{
+    public class UserRepository : Repository
+    {
+        public UserRepository(IDbConnectionProvider connectionProvider) :
+            base(connectionProvider)
+        { }
+
+        public bool Login(User user)
+        {
+            return true;
+        }
+
+        public User GetUserByLogin(string login)
+        {
+            DynamicParameters param = new DynamicParameters();
+            param.Add("@UserLogin", login);
+            return ReturnList<User>("GetUserByLogin", param).FirstOrDefault<User>();
+        }
+
+        public void RegistrationUser(User user)
+        {
+            DynamicParameters param = new DynamicParameters();
+            string salt = BCrypt.Net.BCrypt.GenerateSalt();
+
+            string key = EncryptoUtils.EncryptoUtils.RandomString(50);
+            //string EncryptedFIO = SimpleAES.AES256.Encrypt(user.FIO, key);
+            //string EncryprtedEmail = SimpleAES.AES256.Encrypt(user.Email, key);
+
+            string EncryptedFIO = user.FIO;
+            string EncryprtedEmail = user.Email;
+
+            param.Add("@FIO", EncryptedFIO);
+            param.Add("@Email", EncryprtedEmail);
+            param.Add("@Login", user.Login);
+            param.Add("@Birthday", user.Birthday);
+
+            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(user.UserPassword, salt);
+
+            param.Add("@UserPassword", hashedPassword);
+            //param.Add("@SymmetricKey", key);
+            ExecuteWithoutReturn("RegistrationUser", param);
+        }
+    }
+}
