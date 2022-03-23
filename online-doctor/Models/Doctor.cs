cba2a7972f91ff63e4725fe9cd7b86ff26c430d8
Alexsandr Syrykh <sane4e55@gmail.com>
2022-03-23 22:12:08 +0300

    Дополнил модель доктора.

--- online-doctor/Models/Doctor.cs
+++ online-doctor/Models/Doctor.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace online_doctor.Models
@@ -18,15 +20,27 @@ namespace online_doctor.Models
         [Display(Name = "Фамилия имя отчество")]
         public string FIO { get; set; }
         public string Email { get; set; }
-        public string PathToPhoto { get; set; }
+
+        [Display(Name = "Image")]
+        public string Photo { get; set; }
+        [Display(Name = "Фотография")]
+        public IFormFile ImageFile { get; set; }
+
+        [Display(Name = "Обо мне")]
         public string About { get; set; }
+        [Display(Name = "Образование")]
         public string Education { get; set; }
+        [Display(Name = "Дата рождения")]
         public DateTime Birthday { get; set; }
 
         [Display(Name = "Рейтинг")]
         public float? Rating { get; set; }
         [Display(Name = "Специализация")]
         public string DoctorType { get; set; }
+
+        [Display(Name = "Специализация")]
+        public List<DoctorSpecialization> DoctorSpecializations { get; set; } = new List<DoctorSpecialization>();
+        public int IdDocType { get; set; }
 
         public string ErrorMessage { get; set; }
     }
